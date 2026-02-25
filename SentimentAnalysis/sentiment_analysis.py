2b974009011e9892ead0f40feec6cffb9baa9814
tokinha11 <adrianabadlopez@gmail.com>
2026-02-25 03:41:00 -0500

    complete tasks

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,25 +1,30 @@
-import requests  # Importar la biblioteca requests para manejar solicitudes HTTP
+import requests
 import json
 
 def sentiment_analyzer(text_to_analyse):
-	# URL of the sentiment analysis service
+	# Define la URL para la API de análisis de sentimientos
 	url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
 
-	# Constructing the request payload in the expected format
+	# Crea la carga útil con el texto a analizar
 	myobj = { "raw_document": { "text": text_to_analyse } }
 
-	# Custom header specifying the model ID for the sentiment analysis service
+	# Establece los encabezados con el ID de modelo requerido para la API
 	header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
 
-	# Sending a POST request to the sentiment analysis API
+	# Realiza una solicitud POST a la API con la carga útil y los encabezados
 	response = requests.post(url, json=myobj, headers=header)
 
-	# Parsing the JSON response from the API
+	# Analiza la respuesta de la API
 	formatted_response = json.loads(response.text)
 
-	# Extracting sentiment label and score from the response
-	label = formatted_response['documentSentiment']['label']
-	score = formatted_response['documentSentiment']['score']
+	# Si el código de estado de la respuesta es 200, extrae el label y el score de la respuesta
+	if response.status_code == 200:
+		label = formatted_response['documentSentiment']['label']
+		score = formatted_response['documentSentiment']['score']
+	# Si el código de estado de la respuesta es 500, establece label y score en None
+	elif response.status_code == 500:
+		label = None
+		score = None
 
-	# Returning a dictionary containing sentiment analysis results
+	# Devuelve el label y el score en un diccionario
 	return {'label': label, 'score': score}
